7dd6386376c19a85376f5eb689b4a4274117998f
Aravind <aravindkrishnan05520@gmail.com>
2024-02-25 17:40:53 +0530

    added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { v4 as uuidv4 } from 'uuid';
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   const [todo, setTodo] = useState("");
@@ -14,7 +18,7 @@ function App() {
       setTodos(todos)
     }
   }, [])
-  
+
   const saveToLS = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }
@@ -22,23 +26,45 @@ function App() {
     setTodo(e.target.value);
   }
   const handleSave = () => {
-    setTodos([...todos, { todo, isCompleted: false, id: uuidv4() }]);
-    setTodo("");
-    saveToLS();
+    if(todo.length>=4){
+      setTodos([...todos, { todo, isCompleted: false, id: uuidv4() }]);
+      setTodo("");
+      saveToLS();
+    }
   }
   const handleEdit = (e, id) => {
     let editTodo = todos.filter(item => {
       return item.id === id;
     })
     setTodo(editTodo[0].todo)
-    handleDelete(null, id);
+    handleDelete(null, id, true);
   }
-  const handleDelete = (e, id) => {
-    let newTodos = todos.filter(item => {
-      return item.id !== id;
-    })
-    setTodos(newTodos);
-
+  const handleDelete = (e, id, fromEdit = false) => {
+    let conf = true
+    if (!fromEdit) {
+      let conf = confirm("Are you sure want to delete this todo? This action cannot be undone");
+      toast.error("Todo deleted", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        style: {
+          borderTop: "1px solid rgb(167 139 250)",
+          borderBottom: "1px solid rgb(167 139 250)",
+          backgroundColor: "rgb(15 23 42)",
+          color: "white"
+        }
+      })
+    }
+    if (conf) {
+      let newTodos = todos.filter(item => {
+        return item.id !== id;
+      })
+      setTodos(newTodos);
+    }
   }
   const handleIsComplete = (e) => {
     let id = e.target.name;
@@ -52,18 +78,27 @@ function App() {
   const handleShowCompletedTodos = () => {
     setCompletetedTodos(!completetedTodos)
   }
+  const handleKeydown = (e) => {
+    if(e.key==="Enter"){
+      handleSave();
+    }
+  }
 
   return (
     <>
       <Navbar />
-      <div className="container mx-auto p-5 rounded-xl bg-slate-200 my-5 min-h-[90vh]">
+      <ToastContainer />
+      <div className="md:container mx-auto p-5 rounded-xl bg-slate-400 mt-[4rem] min-h-[90vh] md:w-[70vw] flex flex-col">
         <h1 className="font-bold text-xl">Your todos</h1>
-        <input type="checkbox" name="showCompleted" className="mr-3 my-5" checked={completetedTodos} onChange={handleShowCompletedTodos} />
-        <label htmlFor="showCompleted">Show completed todos</label>
-        <div className="my-5">
-          <input className="outline-none w-1/2 rounded-lg px-4" type="text" name="data" value={todo} onChange={handleInput} />
-          <button className="bg-slate-500 px-2 rounded-md mx-3 text-white disabled:bg-slate-300 transition-all duration-500" onClick={handleSave} disabled={todo.length <= 3}>Save</button>
+        <div>
+          <label htmlFor="showCompleted">Show completed todos</label>
+          <input type="checkbox" id="showCompleted" name="showCompleted" className="mx-3 my-5" checked={completetedTodos} onChange={handleShowCompletedTodos} />
         </div>
+        <div className="my-5">
+          <input className="outline-none w-1/2 rounded-lg px-4 py-2" type="text" name="data" value={todo} onChange={handleInput} onKeyDown={handleKeydown} />
+          <button className="bg-slate-500 px-4 rounded-md mx-3 py-2 text-white disabled:bg-slate-300 transition-all duration-500 hover:bg-green-800" onClick={handleSave} disabled={todo.length <= 3}>Save</button>
+        </div>
+        {todos.length === 0 ? <div>No todos available</div> : ""}
         {todos.map((item) => {
           return (completetedTodos || !item.isCompleted) && <div key={item.id} className="todos">
             <div className="todo flex w-1/2 justify-between my-5">
@@ -72,8 +107,8 @@ function App() {
                 <h2 className={item.isCompleted ? "line-through" : ""}>{item.todo}</h2>
               </div>
               <div className="flex items-center">
-                <button className="bg-slate-500 mx-2 px-2 rounded-md text-white" onClick={(e) => handleEdit(e, item.id)}>edit</button>
-                <button className="bg-slate-500 px-2 rounded-md text-white" onClick={(e) => handleDelete(e, item.id)}>delete</button>
+                <button className="bg-slate-500 mx-2 px-2 rounded-md text-white p-2  hover:bg-green-600 transition-colors duration-500" onClick={(e) => handleEdit(e, item.id)}><MdEdit /></button>
+                <button className="bg-slate-500 px-2 rounded-md text-white p-2 hover:bg-red-600 transition-colors duration-500" onClick={(e) => handleDelete(e, item.id)}><MdDelete /></button>
               </div>
             </div>
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav className='flex bg-slate-900 text-white justify-around py-2'>
+      <nav className='flex bg-slate-900 text-white md:justify-around justify-between py-2 fixed top-0 left-0 w-[100vw]'>
         <div className="logo">
-            <span className='font-bold text-xl mx-9 cursor-pointer'>Taskify 2.0</span>
+            <span className='font-bold md:text-xl md:mx-9 mx-3 cursor-pointer'>Taskify 2.0</span>
         </div>
-        <ul className='flex justify-between gap-4 mx-9'>
-            <li className='cursor-pointer hover:text-red-500 hover:scale-105 transition-all duration-500'>Home</li>
-            <li className='cursor-pointer hover:text-red-500 hover:scale-105 transition-all duration-500'>Your Todos</li>
+        <ul className='flex justify-between gap-4 md:mx-9 mx-3'>
+            <li className='cursor-pointer hover:text-violet-400 hover:scale-105 transition-all duration-500'>Home</li>
+            <li className='cursor-pointer hover:text-violet-400 hover:scale-105 transition-all duration-500'>Your Todos</li>
         </ul>
       </nav>
     </div>
